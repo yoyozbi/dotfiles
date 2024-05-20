13e1bf2c3492dea247f3f8a219029ed71e32d1fe
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-05-20 15:25:43 +0200

    add fullscreen and some floating windows class and fix lid.py on nixos

--- hypr/lid.py
+++ hypr/lid.py
@@ -5,14 +5,18 @@ import json
 import sys
 
 os.chdir(os.path.expanduser("~/.config/hypr"))
+cachePath = "/tmp/.cache"
+
 
 def find_integrated_screen(json):
     for j in json:
         if j["name"] == "eDP-1":
             return j
 
+
 def json_screen_to_string(s):
     return f'{s["name"]},{s["width"]}x{s["height"]}@{s["refreshRate"]},{s["x"]}x{s["y"]},{s["scale"]},transform,{s["transform"]}'
+
 
 if not len(sys.argv) == 2:
     print("Please provide either open or close param")
@@ -20,14 +24,14 @@ if not len(sys.argv) == 2:
 
 action = sys.argv[1]
 if not action == "open" and not action == "close":
-    print(f"Please set \"open\" or \"close\" not \"{action}\"")
+    print(f'Please set "open" or "close" not "{action}"')
     exit(1)
 
 if action == "open":
-    f = open(".cache","r")
+    f = open(cachePath, "r")
     string = f.read()
     f.close()
-    os.system(f"hyprctl keyword monitor \"{string}\"")
+    os.system(f'hyprctl keyword monitor "{string}"')
 elif action == "close":
     raw = subprocess.check_output(["hyprctl", "monitors", "-j"]).decode("utf-8")
     print(raw)
@@ -40,7 +44,7 @@ elif action == "close":
     main = find_integrated_screen(parsed)
 
     string = json_screen_to_string(main)
-    f = open(".cache","w")
+    f = open(cachePath, "w")
     f.write(string)
     f.close()
-    os.system("hyprctl keyword monitor \"eDP-1, disable\"")
+    os.system('hyprctl keyword monitor "eDP-1, disable"')
