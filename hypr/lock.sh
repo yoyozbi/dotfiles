aa4d65b8d8d116dbfc4645c4e4436e63d7911d4d
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-05-21 17:36:37 +0200

    add script for lock

--- hypr/lock.sh
+++ hypr/lock.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 grim -s 1.5 -l 0 ~/.cache/screenlock.png
-convert .cache/screenlock.png -scale 20% -blur 0x2 -resize 200% .cache/screenlock.png
+convert ~/.cache/screenlock.png -blur 0x8 ~/.cache/screenlock.png
 hyprlock
