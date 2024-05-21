3e9406dd28f6c135da7f7e03a3075f63879d4739
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-05-21 18:39:48 +0200

    add script to blur background on hyprlock but got errors when launching from hyprland binds

--- hypr/lock.sh
+++ hypr/lock.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-grim -s 1.5 -l 0 ~/.cache/screenlock.png
+grim -s 1 -l 5 ~/.cache/screenlock.png
 convert ~/.cache/screenlock.png -blur 0x8 ~/.cache/screenlock.png
+err=$?
+if [ $err -ne 0 ]; then
+    echo "[BASH] Error converting $err"
+    cp ~/.config/hypr/SLD24_Wallpaper_4K.png ~/.cache/screenlock.png
+fi
 hyprlock
